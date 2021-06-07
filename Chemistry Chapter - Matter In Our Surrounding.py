99c1c42722aa3750533445f4c8c8d80fbb261ee0
ashutosh708 <85440783+ashutosh708@users.noreply.github.com>
2021-06-07 10:09:24 +0530

    Add files via upload

---
+++ Chemistry Chapter - Matter In Our Surrounding.py
@@ -0,0 +1,204 @@
+print(" Welcome to Chemistry Chapter- Matter In Our Surrounding")
+print("NOTE:- 1.USE SMALL LETTERS ONLY")
+print("NOTE :-2. Do not use any type of signs")
+print ("NOTE:-3.Write every question in the following form: what is matter")
+print("")
+print("")
+b=True
+
+while b==True:
+    a=input("Enter your question related to this chapter at class 9 level").lower()
+
+    if a=='what is matter':
+        print("")
+        print("Anything that occupies space and has mass is called matter")
+
+    elif a== 'give characteristics of matter':
+        print("")
+        print("1.Particles of matter have space between them called intermolecular space")
+        print("2.Particles of matter attract each other by a force called intermolecular force")
+        print("3.Particles of matter moves continuously i.e. particles possess kinetic energy ")
+
+    elif a=='what are the three states of matter ':
+        print("Three states of matter are:-")
+        print("1.Solid")
+        print("2.Liquid")
+        print("3.Gas")
+
+    elif a=='what are characteristics of solid':
+        print("i)Solids have fixed shape and volume")
+        print("ii)solid have neglegible compressibility")
+        print("iii)solids have neglegible kinetic energy because intermolecular force is maximum")
+        print("iv) solids have least intermolecular force")
+        print("v) Solids do not have property of diffusion.")
+        print("vi) Solid are rigid")
+
+    elif a=='what are characteristics of liquid':
+        print("i) Liquids do not have fixed shape but has fixed volume")
+        print("ii) Liquids have fluidity because they have kinetic energy more than solid")
+        print("iii)liquids take the shape of the container in which it is kept")
+        print("iv) Rate of diffusion of liquid is more than solid")
+
+    elif a=='how aquatic animals are able to breathe under water':
+        print("because gases from the atmoshphere diffuse and dissolve in water  and from the water aquatic animals take the gases")
+
+    elif a=='what is diffusion':
+        print("Intermixing of particles of two different states of matter on their own is called diffusion")
+
+    elif a=='what are characteristics of gases':
+        print("i)Gases neither have fixed shape nor fixed volume")
+        print("ii)Gases have maximum fluidity because it has maximum kinetic energy")
+        print("iii)Gases are highly compressible")
+        print("iv)Rate of diffusion is fastest in gases")
+
+    elif a=='which state of matter has maximum intermolecular space':
+        print("Gaseous State")
+        
+    elif a=='which state of matter has minimum intermolecular space':
+        print("solid")
+
+    elif a=='which state of matter has minimum intermolecular force':
+        print("gaseous state")
+
+    elif a=='which state of matter has maximum intermolecular force':
+        print("solid")
+
+    elif a=='which state of matter has maximum kinetic energy':
+        print("gasous state")
+        
+    elif a=='which state of matter has mimimum kinetic energy':
+        print("solid")
+
+        
+    elif a=='which state of matter has minimum compressibility':
+        print("solid")
+
+    elif a=='which state of matter has maximum compressibility':
+        print("gases")
+
+    elif a=='which state of matter has mimimum fluidity':
+        print("solid")
+
+    elif a=='which state of matter has maximum fluidity':
+        print("")
+        print("Gaseous state")
+
+    elif a=='what is change of state of matter':
+        print("")
+        print("The interchange of state of matter from one state to another by increasing or decreasing temperature/pressure is called change of state of matter ")
+
+    elif a=='what is melting':
+        print("")
+        print("The process of change of solid into liquid on heating at melting point is called melting")
+        print("The temperature at which solid changes into liquidis called is melting point")
+        
+    elif a=='what is melting point':
+         print("")
+         print("The temperature at which solid changes into liquidis called melting")
+
+    elif a=='what is fusion':
+        print("")
+        print("The process of melting is also known as fusion")
+
+    elif a=='what is latent heat':
+        print("")
+        print("The heat energy absorbed by matter without showing any rise in temperature is called latent heat")
+        print("")
+        print("")
+        print("The heat supplied to the substance during melting or boiling used up in changing the state by overcoming ")
+        print("intermolecular force of attractionis called latent heat")
+
+    elif a=='what is latent heat of fusion':
+        print("")
+        print("The amount of heat energy that is required to change 1kg solid into liquid at it's melting point id known as latent heat of fusion")
+
+    elif a=='what is boiling':
+        print("")
+        print("The process of change of liquid into gas on heating at boiling point is called boiling")
+        print("The temperature at which liquid changes into gas is called boiling point")
+
+    elif a=='what is boiling point':
+         print("")
+         print("The temperature at which liquid changes into gas is called boiling point")
+         
+    elif a=='what is latent heat of vaporisation':
+        print("")
+        print("The amount of heat energy required to change 1kg of liquid into vapour state at it's boiling point without any rise in temperature ")
+        print("is called latent heat of vapourisation")
+
+    elif a=='what is condensation':
+        print("")
+        print("The process of change of gaseous state to liquid state on cooling is called condensation")
+        
+    elif a=='by which process gaseous state  change into liquid state on cooling ':
+        print("")
+        print("by the process of condensation")
+
+    elif a=='what is freezing':
+        print("")
+        print("The process of change of liquid state to solid state on cooling is called freezing")
+    elif a=='what is sublimation':
+        print("")
+        print("The change of state directly from solid to gas is called sublimation")
+        print("")
+        print("e.g.-napthelene balls")
+
+    elif a=='what is deposition':
+        print("")
+        print("The change of state from gas to solid is called deposition")
+        print("")
+        print("e.g.--dry ice")
+    elif a=='What is the effect of change of pressure':
+        print("")
+        print("By applying high pressure as well as by reducing temperature we can convert gas into liquid and solid")
+
+    elif a=='Why solid carbon dioxide is also known as dry ice':
+        print("")
+        print("because it does not melt into liquid instead it changes into gas by the process of sublimation")
+
+    elif a=='What is the relation between celcius scale and kelvin scale':
+        print("")
+        print("           K=273+degree celcius")
+
+    elif a=='what is evaporation':
+        print("")
+        print("The phenomenon of change of a liquid into vapour at any temperature below it's boiling point is called evaporation")
+        print("")
+        print("Factors which affect the rate of evaporation are:-")
+        print("1.Surface Area")
+        print("2.Temperature")
+        print("3.Wind Speed")
+        print("4.Humidity")
+
+
+    elif a=='show relation between celcius scale and kelvin scale':
+        print("")
+        print("           K=273+degree celcius")
+
+    elif a=='What is the relation between celcius scale and farenheit scale':
+         print("")
+         print("      Farenheit= 9*degree celcius  ")
+         print("                ------------------ + 32      ")
+         print("                      5                 ")
+
+    elif a=='show relation between celcius scale and farenheit scale':
+         print("")
+         print("      Farenheit= 9*degree celcius  + 32")
+         print("                ------------------       ")
+         print("                      5                 ")
+
+    elif a=='what is absolute zero':
+        print("")
+        print("At zero 0K on kelvin scale (-273K) the molecular motion completely stop.This temperature is known as absolute zero")
+        
+         
+    
+    else:
+        print(" result not found")
+
+    c=input("Do you want to repeat the program: yes/no").lower()
+    if c=="yes":
+        continue
+    else:
+        exit()
+
